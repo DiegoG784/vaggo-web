2a0933b9f69f13593e29822b0db2dc175f2c7ab0
DiegoG784 <diegofaria865@gmail.com>
2026-03-27 17:35:28 -0300

    updated files

--- class/user.ts
+++ class/user.ts
@@ -1,4 +1,10 @@
 export default class User {
+
+    public id : string;
+    
+    public constructor(id:string) {
+        this.id = id
+    }
     // set user info here.
     // if putting sensible data here, how can this be achiveable to prevent attacks from outside? also htf is easy to hack this or smth smh
 }
