ce2d831c7c2cb234ebd00d5e950cb0fc117cad1d
DiegoG784 <diegofaria865@gmail.com>
2026-03-28 00:13:25 -0300

    integrated basic api calls into pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/component/header"
 import Link from "next/link"
 // import Image from "next/image";
 import { useRouter, redirect } from "next/navigation" 
+import { useState } from 'react';
 
 
 export default function Page() {
@@ -10,10 +11,62 @@ export default function Page() {
   //  - credentials inserted will be validated and see if email and pass matches from api call
   //  - if approved, insert credentials as cookies or somewhere safe, like a token or smth
   //  - make user credential validation function on controller or model side, idk where tf this stays at i forgor
-  const router = useRouter()
-  const handleRouting = (e:any) => {
+  const [user, setUser] = useState(null)
+  // const router = useRouter()
+  // const handleRouting = (e:any) => {
+    // e.preventDefault()
+    // router.push('/user/dashboard')
+  // }
+
+
+  const handle = (e:any) => {
     e.preventDefault()
-    router.push('/user/dashboard')
+
+    // validar  CPF
+    // validar e-mail
+    // validar telefone
+    // validar senha
+    const formData = new FormData(e.currentTarget)
+    const values = Object.fromEntries(formData)
+    const router = useRouter()
+    
+    console.log(values)
+    let result
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzc0NjYxNTkzLCJleHAiOjE3NzQ2NjUxOTN9.Qd96mltdnSXA9XMGYfawBO3GN3dvloZIJe53jcWXcW4"
+    
+    delete values.passConfirm
+
+    fetch('http://localhost:3000/users/login',
+      {
+        headers: {
+          // 'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(values)
+      }
+    )
+    .then((res) => res.json())
+    .then((data) => {
+      // if response  ok, give token and redirect to page
+      localStorage.setItem('token', data.data.token)
+      console.log(data)
+      // router.push('/user/dashboard')
+
+    })
+
+    // se resposta da api for 200, loga o usuário com a senha criada pelo forms e leva o usuário para a página dashboard
+    console.log(user)
+
+    
+    // transformar dados registro em JSON
+
+    // enviar JSON pra API -> 
+    // <- API envia mensagem 200 (message: true, new_id: token)
+
+
+    // router.push('/user/dashboard')
+
   }
 
   return (
@@ -25,11 +78,11 @@ export default function Page() {
                 <h1>Vaggo</h1>
                 <h1>Seja bem vindo!</h1>
 
-                <form className="flex flex-col" onSubmit={handleRouting}>
+                <form className="flex flex-col" onSubmit={handle}>
                     <label htmlFor="email">Email</label>
                     <input type="text" name="email"/>
 
-                    <label htmlFor="pass">Senha</label>
+                    <label htmlFor="password">Senha</label>
                     <input type="text" name="pass"/>
                     <input type="submit" value={"Entrar"}/>
                 </form>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 import Header from "@/component/header"
+import { The_Nautigal } from "next/font/google";
 import Link from "next/link"
 // import Image from "next/image";
 import { useRouter, redirect } from "next/navigation" 
+// import { redirect } from "next/navigation" 
+import { useEffect, useState } from 'react';
+
+
+
+
 
 
 export default function Page() {
@@ -10,7 +17,9 @@ export default function Page() {
   //  - credentials inserted will be validated and see if email and pass matches from api call
   //  - if approved, insert credentials as cookies or somewhere safe, like a token or smth
   //  - make user credential validation function on controller or model side, idk where tf this stays at i forgor
-  const router = useRouter()
+  // const router = useRouter()
+  
+  const [user, setUser] = useState(null)
 
   const handleRegister = (e:any) => {
     e.preventDefault()
@@ -19,16 +28,63 @@ export default function Page() {
     // validar e-mail
     // validar telefone
     // validar senha
+    const formData = new FormData(e.currentTarget)
+    const values = Object.fromEntries(formData)
+    
+    console.log(values)
+    let result
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzc0NjYxNTkzLCJleHAiOjE3NzQ2NjUxOTN9.Qd96mltdnSXA9XMGYfawBO3GN3dvloZIJe53jcWXcW4"
+    
+    delete values.passConfirm
 
+    fetch('http://localhost:3000/users',
+      {
+        headers: {
+          // 'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(values)
+      }
+    )
+    .then((res) => res.json())
+    .then((data) => setUser(data)).then(
+      () => {
+        let loginData = {
+          email: values.email,
+          password: values.password
+        }
+        console.log(`para login: ${JSON.stringify(loginData)}`)
+        
+        fetch('http://localhost:3000/users/login',
+          {
+            headers: {
+              // 'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(loginData)
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => localStorage.setItem('token', data.data.token))
+      }
+    )
+
+    // se resposta da api for 200, loga o usuário com a senha criada pelo forms e leva o usuário para a página dashboard
+    console.log(user)
+
+    
     // transformar dados registro em JSON
 
     // enviar JSON pra API -> 
     // <- API envia mensagem 200 (message: true, new_id: token)
 
 
-    router.push('/user/dashboard')
+    // router.push('/user/dashboard')
 
   }
+
 
   return (
     <main className="flex flex-col">
@@ -46,10 +102,10 @@ export default function Page() {
                   <input type="text" name="cpf" placeholder="CPF"/>
 
                   <div className="flex flex-row">
-                    <label htmlFor="sex" className="mr-1">Sexo:</label>
-                    <select name="sex">
-                      <option  value={"m"}>Masculino</option>
-                      <option  value={"f"}>Feminino</option>
+                    <label htmlFor="gender" className="mr-1">Sexo:</label>
+                    <select name="gender">
+                      <option  value={"M"}>Masculino</option>
+                      <option  value={"F"}>Feminino</option>
                     </select>
                   </div>
 
@@ -59,9 +115,9 @@ export default function Page() {
                   </div>
 
                   <input type="text" name="email" placeholder="e-mail"/>
-                  <input type="text" name="email" placeholder="(XX) XXXXX-XXXX"/>
+                  <input type="text" name="phone" placeholder="(XX) XXXXX-XXXX"/>
 
-                  <input type="password" name="pass" placeholder="Senha"/>
+                  <input type="password" name="password" placeholder="Senha"/>
 
                   <input type="password" name="passConfirm" placeholder="Digite sua senha novamente"/>
 
